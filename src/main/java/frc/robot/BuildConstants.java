6bda3a08b3e0482405054da65b46fa5e10054cc5
benjamindobbs <benjamindobbs@gmail.com>
2026-03-27 10:44:44 -0400

    Tweaking auto speeds

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "BOP-2026-V2";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 8;
-  public static final String GIT_SHA = "70ea2097902b9f37c0d2a3fbeefbcb813147d79f";
-  public static final String GIT_DATE = "2026-03-27 07:56:43 EDT";
+  public static final int GIT_REVISION = 9;
+  public static final String GIT_SHA = "05caf2551ce8c01fb199e3f475c3d9f45cfd8eff";
+  public static final String GIT_DATE = "2026-03-27 09:43:51 EDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-03-27 09:41:41 EDT";
-  public static final long BUILD_UNIX_TIME = 1774618901253L;
+  public static final String BUILD_DATE = "2026-03-27 09:47:34 EDT";
+  public static final long BUILD_UNIX_TIME = 1774619254948L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
